ffc854b5e325febb45c604e66ad1618ed1eac186
River <river.johnson@stu.barren.kyschools.us>
2024-09-24 08:47:40 -0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Student on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
